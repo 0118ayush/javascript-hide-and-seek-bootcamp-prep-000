c8b2aca8c5c3f08da4d0e753ea7b660831577654
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-10 15:45:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector){
+ const element = document.querySelector(selector);
+ return element
+}
+
+
+
+function nestedTarget(){
+  return document.querySelector("#nested").querySelector(".target")
+}
+
+
+function deepestChild(){
+ return document.querySelector("#grand-node div div div div")
+};
+
+
+function increaseRankBy(n){
+  const li = document.querySelectorAll(".ranked-list"); 
+  for (let i=0; i<li.length; i++){
+    li[i].innerHTML = (i + 1).toString()
+  }
+}
+
 
