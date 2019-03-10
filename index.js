5df3066f8738a6fc6245a1e964a789c3d1455f88
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-10 13:54:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-function getFirstSelector(selector)
+function getFirstSelector(selector){
+  if (selector === id){
+    document.querySelector("#selector");
+  } else if (selector === class){
+    document.querySelector(".selector");
+  }
+}
